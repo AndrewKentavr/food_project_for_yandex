393cb27d5740d94e6180af93f8248e223b6ea39a
Stas <bihug95@gmail.com>
2021-04-27 12:35:57 +0300

    добавлен файл ключей для работы с API обновлён README.md

---
+++ globals.py
@@ -0,0 +1,5 @@
+class Globals:
+    apiKey_spoonacular_1 = '6f7f733b7964497f8f87b57149c43bbc'
+
+    id_yandex = 'b1ghv3pdod31alo8jrrn'
+    apiKey_yandex_translator = 'Api-Key AQVN16eXyhRvpmU7ozpshNnfrZNwmIIsDx2FI6W7'
